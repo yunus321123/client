39454c43e2e78906ced60535cb64e35961a1c5ca
Yunus <118502561+yunus321123@users.noreply.github.com>
2025-08-01 11:33:56 +0530

    Update axiosinstance.js

--- src/Helpers/axiosinstance.js
+++ src/Helpers/axiosinstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL ="http://localhost:5000/api/v1";
+const BASE_URL ="backend-production-79a3.up.railway.app/api/v1";
 
 const axiosInstance = axios.create();
 
